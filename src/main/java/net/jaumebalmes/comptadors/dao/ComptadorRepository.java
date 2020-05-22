61d3c181e8b6d6c3c98d4451e83c24ee116f6cdb
Marti <mjorda18@jaumebalmes.net>
2020-05-22 18:20:42 +0200

    ProyectoFinal Falta Controller i Modificar applicationProperties

--- src/main/java/net/jaumebalmes/comptadors/dao/ComptadorRepository.java
+++
@@ -1,7 +0,0 @@
-package net.jaumebalmes.comptadors.dao;
-
-import net.jaumebalmes.comptadors.model.Comptador;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ComptadorRepository extends CrudRepository<Comptador,Long> {
-}
